1883d5647d662cb0bc092947030e7aa5c4a8e5b0
ASAHI-GH24028 <asahi-gh24028@ASAHI-GH24028s-MacBook-Pro.local>
2025-05-20 23:16:19 +0900

    new file added

--- iOS/appiumhhaproject/src/test/java/StepDefinitions/StepDefinitions.java
+++ iOS/appiumhhaproject/src/test/java/StepDefinitions/StepDefinitions.java
@@ -97,7 +97,7 @@ public class StepDefinitions extends Baseclass {
 	}
 	@Then("Verify app open with onboarding flow")
 	public void Verify_app_open_with_onboarding_flow() throws InterruptedException {
-		    WebElement nextButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//XCUIElementTypeButton[@name=\"次へ1\"]")));
+		    WebElement nextButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//XCUIElementTypeButton[@name=\"次へ\"]")));
 		    nextButton.isDisplayed();
 	}
 	
