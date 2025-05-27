e0a5e7467707cc24653eabcb724e6cc122891f85
ASAHI-GH24028 <asahi-gh24028@ASAHI-GH24028s-MacBook-Pro.local>
2025-05-27 16:35:52 +0900

    new file added

--- iOS/appiumhhaproject/src/test/java/StepDefinitions/StepDefinitions.java
+++ iOS/appiumhhaproject/src/test/java/StepDefinitions/StepDefinitions.java
@@ -155,7 +155,7 @@ public class StepDefinitions extends Baseclass {
 	@Then("Verify Cancel button functionality from pop up")
 	public void Verify_Cancel_button_functionality_from_pop_up() throws InterruptedException {	        
 		WebElement cancel_button = wait.until(ExpectedConditions.visibilityOfElementLocated(
-			    By.xpath("//XCUIElementTypeButton[@name=\"キャンセル\"] | //XCUIElementTypeStaticText[@name=\"Cancel\"]")));
+				By.xpath("//XCUIElementTypeButton[@name=\"キャンセル\" or @name=\"Cancel\"]")));
 	        cancel_button.click();
 	        
 	        WebElement nextButton4 = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//XCUIElementTypeButton[@name=\"サインアップ\"]")));
